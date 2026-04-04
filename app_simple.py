9bf21e331ffdd9b3d7a5967326db2354b7eae06f
cruthilokesh <kruthilokesh1176@gmail.com>
2026-04-04 10:37:29 +0530

    Update app_simple.py

--- app_simple.py
+++ app_simple.py
@@ -283,13 +283,11 @@ def init_database():
     print("Manager: manager / manager123")
     print("Admin: admin / admin123\n")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import os
+
     with app.app_context():
         init_database()
-    app.run(debug=True, host='0.0.0.0', port=5000)
 
-# At the very end of the file, add this:
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
